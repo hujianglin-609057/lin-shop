9e8b0119328fdcc25bea11b588c54978ddf3377d
hujianglin <1165169333@qq.com>
2020-07-09 09:51:32 +0800

    增加

---
+++ shop-operation/src/main/java/com/lin/shop/operation/ShopOperationApplication.java
@@ -0,0 +1,13 @@
+package com.lin.shop.operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOperationApplication.class, args);
+    }
+
+}
---
+++ shop-operation/src/test/java/com/lin/shop/operation/ShopOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.shop.operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
